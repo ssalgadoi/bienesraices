f58bba93a62b93f5d770420542b5720a79161d73
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-21 15:31:08 -0300

    lass="header <?php echo $inicio ? 'inicio' : ''; ?>"

--- index.php
+++ index.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
-                </a>
+<?php
+$inicio = true;
+ include './include/templates/header.php'
+?>
 
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-   
-                
-            </div> <!--.barra-->
-
-            <h1>Venta de Casas y Departamentos  Exclusivos de Lujo</h1>
-        </div>
-    </header>
 
     <main class="contenedor seccion">
         <h1>Más Sobre Nosotros</h1>
