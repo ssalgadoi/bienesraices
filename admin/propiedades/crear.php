7e357287b9fa04b60416ef83611975f7a1350301
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-22 19:34:14 -0300

    Creamos formulario crear propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,47 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
     <a href="/admin/" class="boton boton-verde">Volver</a>
+    
+    <form class="formulario">
+        <fieldset>
+            <legend>Información General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" placeholder="Precio Propiedad">
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png" >
+
+            <label for="descripcion">Descripción:</label>
+            <textarea  id="descripcion"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="estacionamientos">Estacionamientos:</label>
+            <input type="number" id="estacionamientos" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+            <select name="" id="">
+                <option value="1">Juan</option>
+                <option value="2">Karen</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedades" class="boton boton-verde">
+    </form>
 </main>
 
 <?php
