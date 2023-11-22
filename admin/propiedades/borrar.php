99bc94e4f4ca62e5eebf982dba664f4ba3f145fc
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-22 19:11:48 -0300

    Creamos el CRUD

---
+++ admin/propiedades/borrar.php
@@ -0,0 +1,13 @@
+<?php
+    require '../../include/funciones.php';
+    incluirTemplate('header');
+
+?>
+
+<main class="contenedor seccion">
+    <h1>Borrar</h1>
+</main>
+
+<?php
+    incluirTemplate('footer');
+?>
